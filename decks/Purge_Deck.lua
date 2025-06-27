97c235489c52b6f453a5aacbef1cf2a0ff7d12c7
Noodlemire <noodlemire@matthew-B450-AORUS-M.lan>
2025-06-26 21:15:33 -0500

    Added Purge Deck

---
+++ decks/Purge_Deck.lua
@@ -0,0 +1,73 @@
+--[[
+Purge Deck
+
+When Boss Blind is defeated:
+Destroy all playing cards of one random Rank
+Cycles through all possible Ranks, repeating the order if necessary
+
+"The Purge Event is a mysterious",
+"dance party that leads to the",
+"transformations of all guests",
+"into cannibalistic beasts.",
+"Similarly, this deck will",
+"also steadily rid itself of all",
+"inhabitants.",
+"This is very strong for deck-fixing",
+"but there's always the chance that",
+"you lose a bunch of your best cards.",
+"In addition, there is minimal benefit",
+"to using this deck in early Antes."
+--]]
+
+SMODS.Back{
+	key = "purge",
+	loc_txt = {
+		name = "Purge Deck",
+		text = {
+			"When {C:attention}Boss Blind{} is defeated:",
+			"{C:mult}Destroy{} all playing cards",
+			"of one random Rank",
+			"Cycles through all possible Ranks,",
+			"and repeats the order if necessary"
+		}
+	},
+	atlas = "dbbq_decks",
+	pos = {x = 0, y = 0},
+	calculate = function(self, deck, context)
+		if context.round_eval and G.GAME.last_blind and G.GAME.last_blind.boss then
+			if not G.GAME.dbbq_purge_order then
+				local order = {}
+				for k, v in pairs(SMODS.Ranks) do
+					local i = 1 + pseudorandom("That whistle sounds... hungry...") * #order
+					table.insert(order, i, k)
+				end
+				G.GAME.dbbq_purge_order = order
+				print(order)
+			end
+			if not G.GAME.dbbq_purge_index or G.GAME.dbbq_purge_index >= #G.GAME.dbbq_purge_order then
+				G.GAME.dbbq_purge_index = 1
+			else
+				G.GAME.dbbq_purge_index = G.GAME.dbbq_purge_index + 1
+			end
+			local rank = G.GAME.dbbq_purge_order[G.GAME.dbbq_purge_index]
+			local destroy_cards = {}
+			local destroy_indices = {}
+			for i, card in ipairs(G.playing_cards) do
+				if card.base.value == rank then
+					table.insert(destroy_cards, card)
+					draw_card(G.deck, G.play, 1, "up", false, card)
+				end
+			end
+			if #destroy_cards > 0 then
+				G.E_MANAGER:add_event(Event({
+					trigger = "after",
+					delay = 0.1,
+					func = function()
+						SMODS.destroy_cards(destroy_cards)
+						return true
+					end
+				}))
+			end
+		end
+	end
+}
--- main.lua
+++ main.lua
@@ -5,9 +5,23 @@ SMODS.Atlas {
     py = 95,
 }
 
+SMODS.Atlas {
+    key = "dbbq_decks",
+    path = "Decks.png",
+    px = 71,
+    py = 95,
+}
+
 local jokers = NFS.getDirectoryItems(SMODS.current_mod.path.."jokers")
 for _, filename in pairs(jokers) do
     if string.sub(filename, string.len(filename) - 3) == '.lua' then
         assert(SMODS.load_file("jokers/"..filename))()
+    end
+end
+
+local decks = NFS.getDirectoryItems(SMODS.current_mod.path.."decks")
+for _, filename in pairs(decks) do
+    if string.sub(filename, string.len(filename) - 3) == '.lua' then
+        assert(SMODS.load_file("decks/"..filename))()
     end
 end
